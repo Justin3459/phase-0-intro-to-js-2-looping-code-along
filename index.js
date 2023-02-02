f9fd36b293a81045544a0452e8c6b4a5deb9bd04
justin becker <justin3459@hotmail.com>
2023-02-02 11:36:34 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old.Happy birthday to me!`);
+}
+
+const gifts =["teddy bear", "drone", "doll"];
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+wrapGifts(gifts)*/
+
+
+//const name = []
+function writeCards(name, type){
+    const greeting = []
+    for ( let i =0; i < name.length; i ++){
+        const greetingMessage = `Thank you, ${name[i]}, for the wonderful ${type} gift!`
+        greeting.push(greetingMessage)
+        debugger;
+    }
+  return (greeting)
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+//CountDown
+function countDown(){
+    let fromTen = 10
+    while (fromTen >= 0){
+        console.log(fromTen--)
+        //fromTen--
+    }
+
+}
+countDown()
